f9ac9950d3de4e2db2619c63af1d0da6b003b9ba
JosephFleetham <joseph.fleetham@gmail.com>
2019-07-24 15:27:35 -0400

    fixed margins on background

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,12 @@ import TopNav from './TopNav.js';
 
 function App() {
   return (
-    <div className="body">
+    <div className="background">
       <TopNav />
       <div className="pusher">
         <div className="ui vertical masthead center aligned segment">
-          <div id="homebackground">
+          <div id ="body">
+          <div id="textbackground">
             <div className="ui text container">
               <h1 className="ui header">
                 King Bean
@@ -24,75 +25,76 @@ function App() {
                 </h2>
               </div>
             </div>
-          </div>
-          <div id="homeboxfeatured">
-            <h1> Featured Items </h1>
-              <div id="homegridfeatured">
-                <div className="ui two column grid">
-                  <div className="column">
-                  </div>
-                  <div className="column">
+            <div id="homeboxfeatured">
+              <h1> Featured Items </h1>
+                <div id="homegridfeatured">
+                  <div className="ui two column grid">
+                    <div className="column">
+                    </div>
+                    <div className="column">
+                    </div>
                   </div>
                 </div>
+            </div>
+            <div id="homeboxabout">
+              <div className="ui grid">
+                <div className="six wide column">
+                  <img src="" alt="">
+                  </img>
+                </div>
+                <div className="ten wide column">
+                  <h1 className="ui header">
+                  T
+                  </h1>
+                  <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                  </p>
+                  <h3>
+                  </h3>
+                </div>
               </div>
-          </div>
-          <div id="homeboxabout">
-            <div className="ui grid">
-              <div className="six wide column">
-                <img src="" alt="">
-                </img>
+            </div>
+            <div id="homeboxbypa">
+              <div className="ui grid">
+                <div className="eight wide column">
+                  <h1 className="ui header">
+
+                  </h1>
+                  <h2>
+                  Mission Statement:
+                  </h2>
+                  <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                  </p>
+                  <h3>
+                  </h3>
+                </div>
+                <div className="eight wide column">
+                  <img src="" alt="">
+                  </img>
+                </div>
+
               </div>
-              <div className="ten wide column">
-                <h1 className="ui header">
-                T
-                </h1>
-                <p>
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                </p>
-                <h3>
-                </h3>
+            </div>
+            <div id="homeboxcontact">
+              <div className="ui grid">
+                <div className="three wide column"></div>
+                <div className="ten wide column">
+                  <h1 className="ui header">
+                    Contact Me
+                  </h1>
+                  <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                  </p>
+                  <h3>
+                  </h3>
+                </div>
+                <div className="three wide column">
+                </div>
+
               </div>
             </div>
           </div>
-          <div id="homeboxbypa">
-            <div className="ui grid">
-              <div className="eight wide column">
-                <h1 className="ui header">
-
-                </h1>
-                <h2>
-                Mission Statement:
-                </h2>
-                <p>
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                </p>
-                <h3>
-                </h3>
-              </div>
-              <div className="eight wide column">
-                <img src="" alt="">
-                </img>
-              </div>
-
-            </div>
-          </div>
-          <div id="homeboxcontact">
-            <div className="ui grid">
-              <div className="three wide column"></div>
-              <div className="ten wide column">
-                <h1 className="ui header">
-                  Contact Me
-                </h1>
-                <p>
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                </p>
-                <h3>
-                </h3>
-              </div>
-              <div className="three wide column">
-              </div>
-
-            </div>
           </div>
         </div>
       </div>
