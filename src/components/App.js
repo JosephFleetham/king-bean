fe7230a9fe25580470386ed777acae164a4c2509
JosephFleetham <joseph.fleetham@gmail.com>
2019-08-28 18:38:33 -0400

    Started gallery page

--- src/components/App.js
+++ src/components/App.js
@@ -49,6 +49,7 @@ class App extends Component {
     })
   }
   render() {
+    const images = this.state.data;
     if (this.state.home === true) {
       return (
         <div className="background">
@@ -161,92 +162,43 @@ class App extends Component {
            />
           <div className="pusher">
             <div className="ui vertical masthead center aligned segment">
-              <div id ="body">
-              <div id="textbackground">
-                <div className="ui text container">
-                  <h1 className="ui header">
-                    Gallery
-                  </h1>
-                  <h2>
-                    Bean
-                  </h2>
-                  <div id="textbox">
-                  </div>
-                  <div id="hometext">
-                    <h2>
-                    </h2>
-                  </div>
-                </div>
-                <div id="homeboxfeatured">
-                  <h1> Featured Items </h1>
-                    <div id="homegridfeatured">
-                      <div className="ui two column grid">
-                        <div className="column">
+              <div id="body">
+                <div id ="gallerybody">
+                  <div id="list">
+                    <div className="ui three stackable cards">
+                      <div id="box">
+                        <div id="imagecover">
+                          <img src='https://i.imgur.com/jESF2.jpg?1' alt="cover"></img>
                         </div>
-                        <div className="column">
+                      </div>
+                      <div id="box">
+                        <div id="imagecover">
+                          <img src='https://i.imgur.com/jESF2.jpg?1' alt="cover"></img>
+                        </div>
+                      </div>
+                      <div id="box">
+                        <div id="imagecover">
+                          <img src='https://i.imgur.com/jESF2.jpg?1' alt="cover"></img>
+                        </div>
+                      </div>
+                      <div id="box">
+                        <div id="imagecover">
+                          <img src='https://i.imgur.com/jESF2.jpg?1' alt="cover"></img>
+                        </div>
+                      </div>
+                      <div id="box">
+                        <div id="imagecover">
+                          <img src='https://i.imgur.com/jESF2.jpg?1' alt="cover"></img>
+                        </div>
+                      </div>
+                      <div id="box">
+                        <div id="imagecover">
+                          <img src='https://i.imgur.com/jESF2.jpg?1' alt="cover"></img>
                         </div>
                       </div>
                     </div>
-                </div>
-                <div id="homeboxabout">
-                  <div className="ui grid">
-                    <div className="six wide column">
-                      <img src="" alt="">
-                      </img>
-                    </div>
-                    <div className="ten wide column">
-                      <h1 className="ui header">
-                      T
-                      </h1>
-                      <p>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                      </p>
-                      <h3>
-                      </h3>
-                    </div>
                   </div>
                 </div>
-                <div id="homeboxbypa">
-                  <div className="ui grid">
-                    <div className="eight wide column">
-                      <h1 className="ui header">
-
-                      </h1>
-                      <h2>
-                      Mission Statement:
-                      </h2>
-                      <p>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                      </p>
-                      <h3>
-                      </h3>
-                    </div>
-                    <div className="eight wide column">
-                      <img src="" alt="">
-                      </img>
-                    </div>
-
-                  </div>
-                </div>
-                <div id="homeboxcontact">
-                  <div className="ui grid">
-                    <div className="three wide column"></div>
-                    <div className="ten wide column">
-                      <h1 className="ui header">
-                        Contact Me
-                      </h1>
-                      <p>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                      </p>
-                      <h3>
-                      </h3>
-                    </div>
-                    <div className="three wide column">
-                    </div>
-
-                  </div>
-                </div>
-              </div>
               </div>
             </div>
           </div>
